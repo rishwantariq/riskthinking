c942ff8fea31850828d99b8c978a4854d338b700
rishwantariq <rishwan.97@hotmail.com>
2023-05-06 15:42:20 +0500

    fixed routes

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-const MY_APP_BASE_URL = process.env.NODE_ENV === 'production' ? 'https://riskthinking-amber.vercel.app/' : 'http://localhost:3000';
+const MY_APP_BASE_URL = process.env.NODE_ENV === 'production' ? 'https://riskthinking-amber.vercel.app' : 'http://localhost:3000';
 
 export default MY_APP_BASE_URL;
