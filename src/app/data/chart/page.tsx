e9a278b0c8de1dec1fd90772278b2293b178aa20
rishwantariq <rishwan.97@hotmail.com>
2023-05-06 16:41:09 +0500

    minor changes

--- src/app/data/chart/page.tsx
+++ src/app/data/chart/page.tsx
@@ -83,6 +83,10 @@ export default function Page() {
                                 <Process />
                             </div>
                             <ShortBanner gridData={gridData} />
+                            <div style={{marginTop: '6%'}}>
+                                <TopRiskCategories />   
+                            </div>
+
                         </div>   
                     </div>
                     <Divider style={{ width: 'auto', marginLeft: 'auto', marginRight: 'auto', background: 'gray', marginTop: '2%' }} />
@@ -90,7 +94,6 @@ export default function Page() {
                         <Typography color={"white"} mb={'1%'} variant='h1'>Risk Chart</Typography>
                         <Typography color={'white'} mb={'5%'} fontWeight={'regular'} variant='h4'>Powered by Highcharts</Typography>
                         <animated.div ref={chartsRef} style={{ ...spring }}>
-                            <TopRiskCategories />   
                             <LineChart />
                         </animated.div >
                     </div>
